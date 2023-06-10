4af6ccc63f1cd5fa157b4290331d22c7a7560f26
likedidu <91383267+likedidu@users.noreply.github.com>
2023-06-10 08:47:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ EXEC=$(echo $RANDOM | md5sum | head -c 4)
 UUID='de04add9-5c68-8bab-950c-08cd5320df18'
 PASSWORD=fuck
 SecretPATH='/vmess'
+WG_PEER_PUBLIC_KEY=bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=
+WG_PRIVATE_KEY=qDM2ESNy1l1Vk2/VaxXzqFPE4STSMUCIi7xFCD6vw2o=
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
@@ -14,7 +16,7 @@ wget -O - 'https://github.com/SagerNet/sing-box/releases/download/v1.2.7/sing-bo
 install -m 755 ${TMP_DIRECTORY}/sing-box*/sing-box /app/app${EXEC}
 rm -rf ${TMP_DIRECTORY}
 
-sed -i "s#UUID#$UUID#g;s#SecretPATH#${SecretPATH}#g;s#PASSWORD#${PASSWORD}#g;s#WG_PEER_PUBLIC_KEY#${WG_PEER_PUBLIC_KEY}#g;s#WG_PRIVATE_KEY#${WG_PRIVATE_KEY}#g config.json
+sed -i "s#VmessUUID#$VmessUUID#g;s#SecretPATH#${SecretPATH}#g;s#PASSWORD#${PASSWORD}#g;s#WG_PEER_PUBLIC_KEY#${WG_PEER_PUBLIC_KEY}#g;s#WG_PRIVATE_KEY#${WG_PRIVATE_KEY}#g config.json
 sed -i "s#SecretPATH#${SecretPATH}#g /etc/nginx/nginx.conf
 
 rm -rf /usr/share/nginx/*
